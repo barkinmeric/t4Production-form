d88419bc2a51e31041d3d4210b34d62a6729acfa
barkinmeric <barkinmeric@hotmail.com>
2022-10-28 02:10:39 +0300

    mobile texts are added

--- static/image-section.js
+++ static/image-section.js
@@ -1,6 +1,7 @@
 let coordinate = 0;
 let images = document.getElementsByClassName("image");
 let mobileImages = document.getElementsByClassName("mobile-image");
+let mobileImageTexts = document.getElementsByClassName("mobile-image-text");
 let imagesLength = images.length;
 let mobileImagesLength = mobileImages.length;
 let intervalId = null;
@@ -10,9 +11,11 @@ let mobileCounter = 0;
 
 intervalId = window.setInterval(() => {
 	images[counter].classList.remove("active");
-	mobileImages[counter].classList.remove("active");
+	mobileImages[mobileCounter].classList.remove("active");
+	mobileImageTexts[mobileCounter].classList.remove("active");
 	counter = (counter + 1) % imagesLength;
 	mobileCounter = (mobileCounter + 1) % mobileImagesLength;
 	images[counter].classList.add("active");
 	mobileImages[mobileCounter].classList.add("active");
+	mobileImageTexts[mobileCounter].classList.add("active");
 }, autoplay);
